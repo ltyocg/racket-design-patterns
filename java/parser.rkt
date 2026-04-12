dc385f21bb9195a2a6964a4883b83012104dd78b
John Liu <81881627@qq.com>
2026-04-12 16:59:32 +0800

    test

--- java/parser.rkt
+++ java/parser.rkt
@@ -6,7 +6,7 @@
 
 (define java-parser
   (ext-parser
-   [start primary]
+   [start packageName]
    [end EOF]
    [error (lambda (tok-ok? tok-name tok-value start end)
             (error 'java-parser "Parse error at line ~a, col ~a: ~a ~a"
@@ -15,29 +15,18 @@
    [src-pos]
    [tokens empty-tokens tokens]
    [grammar
-    [identifier
-     [(IDENTIFIER) $1]
-     [(MODULE) 'module]
-     [(OPEN) 'open]
-     [(REQUIRES) 'requires]
-     [(EXPORTS) 'exports]
-     [(OPENS) 'opens]
-     [(TO) 'to]
-     [(USES) 'uses]
-     [(PROVIDES) 'provides]
-     [(WHEN) 'when]
-     [(WITH) 'with]
-     [(TRANSITIVE) 'transitive]
-     [(YIELD) 'yield]
-     [(SEALED) 'sealed]
-     [(PERMITS) 'permits]
-     [(RECORD) 'record]
-     [(VAR) 'var]]
-    [primary
-     [(THIS) 'this]
-     [(SUPER) 'super]
-     [(literal) $1]
-     [(identifier) $1]]
+    [packageName ;276
+     [(identifier packageName.2*) (cons $1 $2)]]
+    [packageName.2
+     [(DOT identifier) $2]]
+    [qualifiedNameList ;285
+     [(qualifiedName qualifiedNameList.2*) (cons $1 $2)]]
+    [qualifiedNameList.2
+     [(COMMA qualifiedName) $2]]
+    [qualifiedName ;316
+     [(identifier qualifiedName.2*) (cons $1 $2)]]
+    [qualifiedName.2
+     [(DOT identifier) $2]]
     [literal
      [(integerLiteral) $1]
      [(floatLiteral) $1]
@@ -51,13 +40,45 @@
      [(HEX_LITERAL) (ast-integer-literal $1)]
      [(OCT_LITERAL) (ast-integer-literal $1)]
      [(BINARY_LITERAL) (ast-integer-literal $1)]]
-    [floatLiteral
+    [floatLiteral ;337
      [(FLOAT_LITERAL) (ast-float-literal $1)]
-     [(HEX_FLOAT_LITERAL) (ast-float-literal $1)]]]
-   ))
+     [(HEX_FLOAT_LITERAL) (ast-float-literal $1)]]
+    [identifier ;480
+     [(IDENTIFIER) $1]
+     [(MODULE) "module"]
+     [(OPEN) "open"]
+     [(REQUIRES) "requires"]
+     [(EXPORTS) "exports"]
+     [(OPENS) "opens"]
+     [(TO) "to"]
+     [(USES) "uses"]
+     [(PROVIDES) "provides"]
+     [(WHEN) "when"]
+     [(WITH) "with"]
+     [(TRANSITIVE) "transitive"]
+     [(YIELD) "yield"]
+     [(SEALED) "sealed"]
+     [(PERMITS) "permits"]
+     [(RECORD) "record"]
+     [(VAR) "var"]]
+    [primary ;710
+     [(THIS) 'this]
+     [(SUPER) 'super]
+     [(literal) $1]
+     [(identifier) $1]]
+    [primitiveType ;799
+     [(BOOLEAN) 'boolean]
+     [(CHAR) 'char]
+     [(BYTE) 'byte]
+     [(SHORT) 'short]
+     [(INT) 'int]
+     [(LONG) 'long]
+     [(FLOAT) 'float]
+     [(DOUBLE) 'double]]
+    ]))
 (define (parse-java-code input)
   (define port (open-input-string input))
   (java-parser (lambda () (java-lexer port))))
 (module+ main
   (require racket/pretty)
-  (pretty-print (parse-java-code "var")))
+  (pretty-print (parse-java-code "ab.dd2.to")))
